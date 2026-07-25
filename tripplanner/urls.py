b4ab928c2c180f6d60a1a8a9f874c8fe71087a4a
Ahmad Zakwan <azakwan2006@gmail.com>
2026-07-25 15:59:56 +0500

    Fix urls.py files mixed up between tripplanner and truckbackend

--- tripplanner/urls.py
+++ tripplanner/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import calculate_trip
+from .views import calculate_trip, test_outbound
 
 urlpatterns = [
     path("calculate-trip/", calculate_trip, name="calculate-trip"),
+    path("test-outbound/", test_outbound, name="test-outbound"),
 ]
